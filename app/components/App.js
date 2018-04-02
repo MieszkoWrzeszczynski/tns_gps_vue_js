b184194a905a72a8c3ee43b54206891dcdea9447
Mieszko <mieszkobor@op.pl>
2018-04-02 12:02:34 +0200

    Fix

--- app/components/App.js
+++ app/components/App.js
@@ -1,8 +1,5 @@
 const Vue = require("nativescript-vue");
 const geoLocation = require("nativescript-geolocation");
-const mapbox = require("nativescript-mapbox");
-const platform = require("platform");
-const isIOS = platform.device.os === platform.platformNames.ios;
 
 module.exports = {
   methods: {
@@ -50,42 +47,21 @@ module.exports = {
   },
 
   template: `
-<Page xmlns="http://schemas.nativescript.org/tns.xsd" xmlns:map="nativescript-mapbox" navigatingTo="navigatingTo">
-  <StackLayout>
-   <Button text="Show location" @tap="enableLocationServices" />
-    <Label text="Nice map, huh!" class="title"/>
-    <ContentView height="240" width="240">
-      <!-- IMPORTANT: plugin version 3 uses :MapboxView, lower versions use :Mapbox -->
-      <map:MapboxView
-          accessToken="pk.eyJ1IjoibWllc3prbyIsImEiOiJjamZoOW5kZGUzNnB2M3VvMW1jaHAwdTc2In0.DsuHqmKEPQSbGbFIddNuxA"
-          mapStyle="traffic_night"
-          latitude="52.3702160"
-          longitude="4.8951680"
-          zoomLevel="3"
-          showUserLocation="true"
-          mapReady="onMapReady">
-      </map:MapboxView>
-    </ContentView>
-  </StackL
-    // <Page class="page">
-//       <ActionBar title="Home" class="action-bar" />
-//       <ScrollView>
-//         <StackLayout class="home-panel">
-//           <!--Add your page content here-->
-// <StackLayout>
-//   <Button text="Show location" @tap="enableLocationServices" />
-//             <StackLayout :visibility="currentGeoLocation.latitude ? 'visible' : 'collapsed'">
-//               <Label :text="'Latitude: ' + currentGeoLocation.latitude" />
-//               <Label :text="'Longitude: ' + currentGeoLocation.longitude" />
-//               <Label :text="'Altitude: ' + currentGeoLocation.altitude" />
-//               <Label :text="'Direction: ' + currentGeoLocation.direction" />
-//             </StackLayout>
-// <ContentView height="240" width="240">
-//       <!-- IMPORTANT: plugin version 3 uses :MapboxView, lower versions use :Mapbox -->
-
-//           </StackLayout>
-//         </StackLayout>
-//       </ScrollView>
-//     </Page>
+    <Page class="page">
+      <ActionBar title="Home" class="action-bar" />
+      <ScrollView>
+        <StackLayout class="home-panel">
+          <StackLayout>
+          <Button text="Show location" @tap="enableLocationServices" />
+            <StackLayout :visibility="currentGeoLocation.latitude ? 'visible' : 'collapsed'">
+              <Label :text="'Latitude: ' + currentGeoLocation.latitude" />
+              <Label :text="'Longitude: ' + currentGeoLocation.longitude" />
+              <Label :text="'Altitude: ' + currentGeoLocation.altitude" />
+              <Label :text="'Direction: ' + currentGeoLocation.direction" />
+            </StackLayout>
+          </StackLayout>
+        </StackLayout>
+      </ScrollView>
+    </Page>
   `
 };
